e8cd66a030ead0b6ab4e846fb135074e4d1a9202
Tianle Zhu <ztl20000828@gmail.com>
2024-04-18 16:56:03 -0400

    Newest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import Navbar from './Component/Navbar';
+import Home from './Component/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Create from './Component/Create';
+import ApplicationDetails from './Component/ApplicationDetails'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/apps/update/:id" element={<ApplicationDetails />} /> 
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
